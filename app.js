5cd2ccdb71d6296a394a5fd15619586a50aaaac9
Nexusf1re <matteussantos30@hotmail.com>
2024-10-03 23:06:19 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const { Telegraf } = require('telegraf');
+
+// Substitua pelo token do seu bot
+const bot = new Telegraf(process.env.BOT_TOKEN || 'SEU_TOKEN_AQUI');
+
+// Comando /Aplicativo
+bot.command('Aplicativo', (ctx) => {
+    ctx.reply('Você acionou o comando /Aplicativo! Aqui estão as informações sobre o aplicativo:');
+    ctx.reply('Clique no link abaixo para acessar o Sistema Financeiro:\nhttps://t.me/n3xuss_bot/SistemaFinanceiro');
+});
+
+// Outros comandos
+bot.start((ctx) => {
+    ctx.reply('Bem-vindo ao bot! Use /Aplicativo para saber mais sobre o aplicativo.');
+});
+
+bot.command('help', (ctx) => {
+    ctx.reply('Aqui estão os comandos disponíveis:\n/start - Inicia o bot\n/help - Mostra esta mensagem de ajuda\n/Aplicativo - Informações sobre o aplicativo');
+});
+
+// Inicia o bot
+bot.launch();
+console.log('Bot está rodando...');
