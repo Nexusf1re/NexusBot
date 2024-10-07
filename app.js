a3b588c7d8ad29857e686452bc1eb32a9f88349c
Nexusf1re <matteussantos30@hotmail.com>
2024-10-07 20:01:43 -0300

    dependencies imported

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+require('dotenv').config(); // Carrega as variáveis de ambiente do .env
 const express = require('express');
 const { Telegraf } = require('telegraf');
 
 const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Middleware para processar atualizações do Telegram
-app.use('/webhook', (req, res) => {
-    console.log('Recebido webhook:', req.body);
-    bot.webhookCallback('/webhook')(req, res);
-    res.sendStatus(200); // Responde ao Telegram que a requisição foi recebida
-});
+// Middleware para processar atualizações
+app.use(express.json());
+app.use(bot.webhookCallback('/webhook'));
 
 // Defina os comandos e respostas
 bot.start((ctx) => ctx.reply('Bem-vindo ao bot!'));
 bot.command('aplicativo', (ctx) => {
     ctx.reply('Clique no link para acessar o Sistema Financeiro: https://t.me/n3xuss_bot/SistemaFinanceiro');
+});
+
+// Middleware para logar o webhook
+app.post('/webhook', (req, res) => {
+    console.log('Recebido webhook:', req.body);
+    res.sendStatus(200); // Responda ao Telegram com 200 OK
 });
 
 // Inicia o servidor
